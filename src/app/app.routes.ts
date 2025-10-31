4c4a32cbaaf4e1f5a783a04a6573015e638e7f6c
Ahmed Elamir <ahmed@example.com>
2025-10-31 17:01:16 +0200

    upload

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -23,29 +23,28 @@
 // })
 // export class AppRoutingModule { }   
 
+import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
+// استدعاء الكمبوننتات
 import { Home } from './home/home';
 import { About } from './about/about';
 import { Products } from './products/products';
-import { NgModule } from '@angular/core';
 import { Login } from './login/login';
-import { inject } from '@angular/core';
-import { Router } from '@angular/router';
-// ✅ الاسم الصحيح
 import { AdminDashboardComponent } from './admin-dashboard/admin-dashboard';
 
 export const routes: Routes = [
-    { path: 'login', component: Login },
-    { path: 'Home', component: Home },
-    { path: 'About', component: About },
-    { path: 'Products', component: Products },
-    // ✅ استخدام الاسم الصحيح
-    { path: 'admin-dashboard', component: AdminDashboardComponent },
-    { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: Login },
+  { path: 'Home', component: Home },
+  { path: 'About', component: About },
+  { path: 'Products', component: Products },
+  { path: 'admin-dashboard', component: AdminDashboardComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  // ✅ استخدم HashLocationStrategy لتجنب 404 على GitHub Pages
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
